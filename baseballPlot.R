c5b5187c220a8750bf02d705a4e4e97e49b96ebc
Gielpy <leverette.welsh@gmail.com>
2015-04-17 16:49:31 -0700

    Testing plot by League

--- baseballPlot.R
+++ baseballPlot.R
@@ -7,7 +7,7 @@ load_data <- function(path) {
   do.call(rbind, tables)
 }
 
-test<-load_data('~/Baseball')
+test<-load_data('~/GitHub/baseballGameTime')
 
 summarySE(test, measurevar='gameTime', groupvars=c('Year'))
 
@@ -24,3 +24,8 @@ ggplot(test, aes(factor(Year), gameTime))+
 ggplot(test, aes(factor(Home), gameTime))+
   geom_boxplot()+
   facet_wrap('Year', nrow=8)
+
+#box plot of average game time by league
+summarySE(test, measurevar='gameTime', groupvars=c('League'))
+ggplot(test, aes(factor(League), gameTime))+
+  geom_boxplot()
